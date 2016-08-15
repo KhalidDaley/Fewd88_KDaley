e42be99a446f267d4511325daa9fc4200228adda
KhalidDaley <Khalid.Daley@gmail.com>
2016-08-15 17:20:05 -0400

    final project 3 added

--- Final Project 3/js/app.js
+++ Final Project 3/js/app.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
 	});
 })
 
-
+//  slideshow for cover photos
 var slideIndex = 0;
 showSlides();
 
@@ -23,5 +23,17 @@ function showSlides() {
     slideIndex++;
     if (slideIndex> slides.length) {slideIndex = 1} 
     slides[slideIndex-1].style.display = "block"; 
-    setTimeout(showSlides, 5000); // Change image every 2 seconds
+    setTimeout(showSlides, 5000); // Change image every 5 seconds
 }
+
+// slideshow for advertisements
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#adslideshow > div:first')
+    .fadeOut(500)
+    .next()
+    .fadeIn(500)
+    .end()
+    .appendTo('#adslideshow');
+},  1000);
